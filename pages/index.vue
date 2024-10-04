d0ff422b0fd4840a2ef9e0e11cce5022a7fdd987
Namphet <2023017@auton1108.onmicrosoft.com>
2024-10-04 13:21:48 +0700

    new version

--- pages/index.vue
+++ pages/index.vue
@@ -12,8 +12,6 @@
       <BannerSub />
 
       <IntroductionLao />
-
-      <!-- <Journey /> -->
     </main>
 
     <!-- Footer -->
@@ -27,7 +25,6 @@ import BannerMain from "@/components/bannermain/bannermain.vue";
 import GetTravel from "@/components/gettravel/gettravel.vue";
 import BannerSub from "~/components/bannersub/bannersub.vue";
 import IntroductionLao from "@/components/introductionlao/introductionlao.vue";
-// import Journey from "~/components/journey /journey.vue";
 import Footer from "@/components/footer/footer.vue";
 </script>
 
