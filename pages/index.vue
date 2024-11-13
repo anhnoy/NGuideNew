4722500ba81c5ab2f1b462b8911eb5d84a3cc87e
Khanxay <2024007@auton1108.onmicrosoft.com>
2024-11-13 17:23:53 +0700

    update develop 20241113

--- pages/index.vue
+++ pages/index.vue
@@ -20,12 +20,26 @@
 </template>
 
 <script setup>
+useHead({
+  title: 'autontour', // Optional, you can set a custom title for the homepage
+  meta: [
+    {
+      name: 'naver-site-verification',
+      content: '8eaa0adf55c96d5b0b83954be26d188fa8aa6866',
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1',
+    },
+  ],
+})
 import Navbar from "@/components/navbar/navbar.vue";
 import BannerMain from "@/components/bannermain/bannermain.vue";
 import GetTravel from "@/components/gettravel/gettravel.vue";
 import BannerSub from "~/components/bannersub/bannersub.vue";
 import IntroductionLao from "@/components/introductionlao/introductionlao.vue";
 import Footer from "@/components/footer/footer.vue";
+
 </script>
 
 <style scoped></style>
