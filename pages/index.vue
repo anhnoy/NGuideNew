9a0d17b241a8aabb431206eb526fc25a3d272e22
Khanxay <2024007@auton1108.onmicrosoft.com>
2024-11-25 16:20:45 +0700

    update meta tags

--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,23 @@
 </template>
 
 <script setup>
+useHead({
+  title: 'autontour', // Optional, you can set a custom title for the homepage
+  meta: [
+    {
+      name: 'naver-site-verification',
+      content: '8eaa0adf55c96d5b0b83954be26d188fa8aa6866',
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1',
+    },
+    { property: 'og:title', content: 'auto&tour' },
+    { property: 'og:description', content: '당신이 만들어 가는 여행, 라오스 맞춤 여행 전문 오토앤투어' },
+    { property: 'og:image', content: 'https://autontour.com/assets/images/AutonTour_logo.png' }, // Replace with your image URL
+    { property: 'og:url', content: 'https://autontour.com/' }
+  ],
+})
 import Navbar from "@/components/navbar/navbar.vue";
 import BannerMain from "@/components/bannermain/bannermain.vue";
 import GetTravel from "@/components/gettravel/gettravel.vue";
