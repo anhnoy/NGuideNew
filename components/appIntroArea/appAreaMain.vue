6c7e6b496373ddeea626ac93a96f6960a9c2c275
Dong-hunter <donghunter48@gmail.com>
2025-04-11 15:58:49 +0700

    fix app and icon

--- components/appIntroArea/appAreaMain.vue
+++ components/appIntroArea/appAreaMain.vue
@@ -18,26 +18,64 @@
         </button>
       </div>
   
-      <!-- Images -->
-      <div class="mx-auto flex justify-center lg:mt-10">
-        <img
-          v-if="selected === 1"
-          src="../../assets/images/app-area1.png"
-          alt="위치와 거리 확인"
-          class="lg:w-[1280px] lg:h-[800px] w-[328px] h-[205px]"
-        />
-        <img
-          v-if="selected === 2"
-          src="../../assets/images/app-area2.png"
-          alt="여행 일정 확인"
-          class="lg:w-[1280px] lg:h-[800px] w-[328px] h-[205px]"
-        />
-        <img
-          v-if="selected === 3"
-          src="../../assets/images/app-area3.png"
-          alt="가이드와 실시간 소통"
-          class="lg:w-[1280px] lg:h-[800px] w-[328px] h-[205px]"
-        />
+      <div class="bg-[#EFEFEF] lg:w-[1280px] lg:h-[800px] w-[328px] h-[205px] mx-auto flex justify-center flex-col">
+        <div>
+          <div class="flex items-center justify-center gap-x-[40px] lg:gap-x-[150px]">
+            <div class="flex items-center">
+              <div>
+                <div v-if="selected === 1" class="text-[10px] lg:text-[30px] font-bold">
+                  <p>나의 위치에서 다음 목적지까지의</p>
+                  <p>거리를 확인해 보세요.</p>
+                </div>
+                <div v-if="selected === 2" class="text-[10px] lg:text-[30px] font-bold">
+                  <p>여행 일정을 한 눈에 볼 수 있어요.</p>
+                </div>
+                <div v-if="selected === 3" class="text-[10px] lg:text-[30px] font-bold">
+                  <p>가이드 정보를 확인하고 전화로</p>
+                  <p>연락할 수 있어요.</p>
+                </div>
+                <div class="lg:mt-8">
+                  <div class="lg:space-x-5 space-x-3 lg:h-[70px] h-[40px] flex items-center justify-center">
+                    <button class="rounded-lg lg:w-[170px] lg:h-[58px] w-[44px] h-[15px]" @click="goToAppStore">
+                      <img src="@/assets/icons/Appstore.svg" class="lg:w-[170px] lg:h-[58px]" />
+                    </button>
+                    <button class="rounded-lg lg:w-[170px] lg:h-[58px] w-[44px] h-[15px]" @click="goToPlayStore">
+                      <img src="@/assets/icons/GooglePlay.svg" class="lg:w-[170px] lg:h-[58px] w-[44px] h-[15px]" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <!-- Images -->
+              <div class="flex justify-center lg:mt-10">
+                <img
+                  v-if="selected === 1"
+                  src="../../assets/images/app-area1.png"
+                  alt="위치와 거리 확인"
+                  class="lg:w-[345px] lg:h-[708px] w-[89px] h-[183px]"
+                />
+                <img
+                  v-if="selected === 2"
+                  src="../../assets/images/app-area2.png"
+                  alt="여행 일정 확인"
+                  class="lg:w-[345px] lg:h-[708px] w-[89px] h-[183px]"
+                />
+                <img
+                  v-if="selected === 3"
+                  src="../../assets/images/app-area3.png"
+                  alt="가이드와 실시간 소통"
+                  class="lg:w-[345px] lg:h-[708px] w-[89px] h-[183px]"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="lg:text-[16px] text-[6px] float-start lg:ml-5 ml-2">
+            <p>nGuide APP은 여행 확정 후 이용하실 수 있습니다.</p>
+          </div>
+        </div>
       </div>
     </div>
   </template>
@@ -51,5 +89,11 @@
     '여행 일정 확인',
     '가이드와 실시간 소통',
   ]
+  const goToAppStore = ()=>{
+    window.open("https://apps.apple.com/kr/app/%EC%95%A4%EA%B0%80%EC%9D%B4%EB%93%9C-nguide/id6738574891", "_blank")
+  };
+  const goToPlayStore = ()=>{
+    window.open("https://play.google.com/store/apps/details?id=com.andguide&pli=1", "_blank")
+  };
   </script>
   
