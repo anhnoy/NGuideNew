581181fb1df4c21468c031ee908cd8f3b3c4305b
Dong-hunter <donghunter48@gmail.com>
2025-04-18 11:03:23 +0700

    update design

--- components/soloTravelSubMain/StepArrow.vue
+++ components/soloTravelSubMain/StepArrow.vue
@@ -2,11 +2,11 @@
   <div class="flex items-center justify-center text-gray-400">
     <!-- Show horizontal arrow on desktop -->
     <span class="hidden lg:inline">
-     <img src="../../assets/icons/arrow-right.svg" alt="arrow-right">
+     <img src="../../assets/icons/Arrow2.png" alt="arrow-right">
     </span>
     <!-- Show vertical arrow on mobile -->
     <span class="lg:hidden ">
-      <img src="../../assets/icons/arrow-right.svg" alt="arrow-right">
+      <img src="../../assets/icons/Arrow2.png" alt="arrow-right">
     </span>
   </div>
 </template>
